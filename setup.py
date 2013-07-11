68b4b7b14a94cd5f57786e68fce6a87c2958bfb6
Kim Grasman <kim.grasman@gmail.com>
2013-07-11 16:46:50 +0200

    First stab at packaging.

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='asn1ate',
+    version='0.4',
+    description='ASN.1 translation library.',
+    author='Kim Gräsman',
+    author_email='kim.grasman@gmail.com',
+    license='BSD',
+    long_description=open('README.txt').read(),
+    url='http://github.com/kimgr/asn1ate',
+    packages=[
+        'asn1ate',
+        'asn1ate.support',
+    ],
+    platforms=['any'],
+    requires=[
+        'pyparsing (>=2.0.0)',
+    ]
+)
