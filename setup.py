f3b66aa2fe4dd9fdabb05f17aa909c8bba0576c4
Kim Grasman <kim.grasman@gmail.com>
2013-08-05 10:01:24 +0200

    Spice up packaging a little.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     author='Kim Gräsman',
     author_email='kim.grasman@gmail.com',
     license='BSD',
-    long_description=open('README.txt').read(),
+    long_description=open('README.rst').read(),
     url='http://github.com/kimgr/asn1ate',
     packages=[
         'asn1ate',
