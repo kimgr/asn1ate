kimgr/asn1ate

commit dce1d3653fdad55209f8f96a83c3242a9abefe06

Test harness now invokes command-line driver
The test harness would reimplement the command-line driver in a simpler
form. That was nice, but would also mean that we had no test coverage
for the driver.

Turn it around so that the test harness now calls the parameterized
driver main.

No functional change intended.

asn1ate/pyasn1gen.py
@@ -616,14 +616,7 @@ def _sanitize_module(name):
 
 
 # Simplistic command-line driver
-def main():
-    arg_parser = argparse.ArgumentParser(description='Generate Python classes from an ASN.1 definition file.'
-                                                     'Output to stdout by default.')
-    arg_parser.add_argument('file', help='the ASN.1 file to process')
-    arg_parser.add_argument('--split', action='store_true',
-                            help='output multiple modules to separate files')
-    args = arg_parser.parse_args()
-
+def main(args):
     with open(args.file, 'r') as data:
         asn1def = data.read()
 
@@ -654,4 +647,11 @@ def main():
 
 
 if __name__ == '__main__':
-    sys.exit(main())
+    arg_parser = argparse.ArgumentParser(
+        description=('Generate Python classes from an ASN.1 definition file. '
+                     'Output to stdout by default.'))
+    arg_parser.add_argument('file', help='the ASN.1 file to process')
+    arg_parser.add_argument('--split', action='store_true',
+                            help='output multiple modules to separate files')
+    args = arg_parser.parse_args()
+    sys.exit(main(args))

asn1ate/test.py
@@ -28,8 +28,7 @@ from __future__ import print_function  # Python 2 compatibility
 import os
 import sys
 import argparse  # Requires Python 2.7 or later, but that's OK for a test driver
-from asn1ate import parser, sema, pyasn1gen, __version__
-from asn1ate.support import pygen
+from asn1ate import parser, sema, pyasn1gen
 
 
 def parse_args():
@@ -47,22 +46,19 @@ def parse_args():
     return ap.parse_args()
 
 
-def generate_code_to_file(input_name, module, modules, file):
-    header = pygen.auto_generated_header(input_name, __version__)
-    pyasn1gen.generate_pyasn1(module, file, modules, header=header)
+def generate_module_code(args):
+    # Absolutize input path before changing working directory
+    infile = os.path.abspath(args.file)
+    split = bool(args.outdir)
 
+    prev_cwd = os.getcwd()
+    try:
+        if split:
+            os.chdir(args.outdir)
 
-def generate_module_code(args, module, modules):
-    if not args.outdir:
-        generate_code_to_file(args.file, module, modules, sys.stdout)
-    else:
-        output_file = pyasn1gen._sanitize_module(module.name) + '.py'
-        output_file = os.path.join(args.outdir, output_file)
-        if os.path.exists(output_file):
-            raise Exception('ERROR: output file %s already exists' % output_file)
-
-        with open(output_file, 'w') as file:
-            generate_code_to_file(args.file, module, modules, file)
+        pyasn1gen.main(argparse.Namespace(file=infile, split=split))
+    finally:
+        os.chdir(prev_cwd)
 
 
 # Simplistic command-line driver
@@ -87,8 +83,7 @@ def main():
         return 0
 
     if args.gen:
-        for module in modules:
-            generate_module_code(args, module, modules)
+        generate_module_code(args)
 
     return 0
 

commit 131411bca348ecce712623ed3fcb5277465bebc5

Make the asn1ate command work again.
The asn1ate entry-point was broken in commit
dce1d3653fdad55209f8f96a83c3242a9abefe06, when an `args`
argument was added to `main`.

Wire things to make it work again.

asn1ate/pyasn1gen.py
@@ -667,7 +667,7 @@ def main(args):
     return 0
 
 
-if __name__ == '__main__':
+def main_cli():
     arg_parser = argparse.ArgumentParser(
         description=('Generate Python classes from an ASN.1 definition file. '
                      'Output to stdout by default.'))
@@ -675,4 +675,8 @@ if __name__ == '__main__':
     arg_parser.add_argument('--split', action='store_true',
                             help='output multiple modules to separate files')
     args = arg_parser.parse_args()
-    sys.exit(main(args))
+    return main(args)
+
+
+if __name__ == '__main__':
+    sys.exit(main_cli())

setup.py
@@ -48,7 +48,7 @@ setup(
     ],
     entry_points={
         'console_scripts': [
-            'asn1ate = asn1ate.pyasn1gen:main'
+            'asn1ate = asn1ate.pyasn1gen:main_cli'
         ]
     }
 )